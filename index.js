1f62794ac7c343787f21e55bff45a2432e466722
roberto.torres <ts.roberto@gmail.com>
2020-06-24 14:55:14 +0100

    adding .prettierrc

--- index.js
+++ index.js
@@ -1,72 +1,71 @@
 #!/usr/bin/env node
-"use strict";
+'use strict'
 
-var path = require("path");
-var fs = require("fs");
-var Transform = require("stream").Transform;
-var getStdin = require("get-stdin");
+var path = require('path')
+var fs = require('fs')
+var Transform = require('stream').Transform
 
-var args = require("minimist")(process.argv.slice(2), {
-  boolean: ["help", "in", "out"],
-  string: ["file"],
-});
+var args = require('minimist')(process.argv.slice(2), {
+  boolean: ['help', 'in', 'out'],
+  string: ['file'],
+})
 
-const BASE_PATH = path.resolve(process.env.BASE_PATH || __dirname);
-const OUTFILE = path.join(BASE_PATH, "out.txt");
+const BASE_PATH = path.resolve(process.env.BASE_PATH || __dirname)
+const OUTFILE = path.join(BASE_PATH, 'out.txt')
 
 if (args.help || process.argv.length <= 2) {
-  error(null, true);
-} else if (args._.includes("-") || args.in) {
-  processFile(process.stdin);
+  error(null, true)
+} else if (args._.includes('-') || args.in) {
+  processFile(process.stdin)
 } else if (args.file) {
-  let stream = fs.createReadStream(path.join(BASE_PATH, args.file));
-  processFile(stream);
+  let stream = fs.createReadStream(path.join(BASE_PATH, args.file))
+  processFile(stream)
 } else {
-  error("Incorrect usage.", true);
+  error('Incorrect usage.', true)
 }
 
 // -------------------------------
 
 function processFile(inStream) {
-  var outStream = inStream;
+  var outStream = inStream
 
   var upperStream = new Transform({
     transform(chunk, enc, callbk) {
       // process.stdout.write(chunk);
-      this.push(chunk.toString().toUpperCase());
-      callbk();
+      this.push(chunk.toString().toUpperCase())
+      callbk()
     },
-  });
+  })
 
-  outStream = outStream.pipe(upperStream);
+  outStream = outStream.pipe(upperStream)
 
-  var targetStream;
+  var targetStream
 
   if (args.out) {
-    targetStream = process.stdout;
+    targetStream = process.stdout
   } else {
-    targetStream = fs.createWriteStream(OUTFILE);
+    targetStream = fs.createWriteStream(OUTFILE)
   }
 
-  outStream.pipe(targetStream);
+  outStream.pipe(targetStream)
 }
 
 function error(err, showHelp = false) {
-  process.exitCode = 1;
-  console.error(err);
+  process.exitCode = 1
+  console.error(err)
 
   if (showHelp) {
-    console.log("");
-    printHelp();
+    console.log('')
+    printHelp()
   }
 }
 
 function printHelp() {
-  console.log("ic test usage:");
-  console.log("");
-  console.log("  --help                      print this help");
-  console.log("  --in, -                     process in"); // cat ./customers.txt | ./index.js -
-  console.log("  --out                       print to stdout");
-  console.log("  --file={FILENAME}           read file from {FILENAME}");
-  console.log("");
+  console.log('ic test usage:')
+  console.log('')
+  console.log('  --help                      print this help')
+  console.log('  --in, -                     process in') // cat ./customers.txt | ./index.js -
+  console.log('  --out                       print to stdout')
+  console.log('  --file={FILENAME}           read file from {FILENAME}')
+  console.log('')
 }
