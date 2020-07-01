d9d69d2f700a49c67927fe4a9a869b8539556f7e
roberto.torres <ts.roberto@gmail.com>
2020-07-01 01:51:57 +0100

    styling electron app

--- app/renderer/renderer.js
+++ app/renderer/renderer.js
@@ -4,9 +4,11 @@ const mainProcess = remote.require('./main.js')
 const btnOpen = document.getElementById('btnOpen')
 const btnSave = document.getElementById('btnSave')
 const userlist = document.getElementById('userlist')
+const output = document.getElementsByClassName('output')[0]
 const selectedfile = document.getElementById('file')
 
 btnOpen.addEventListener('click', () => {
+  userlist.innerHTML = ''
   mainProcess.getFileFromUserAndProcess()
 })
 
@@ -15,8 +17,23 @@ btnSave.addEventListener('click', () => {
 })
 
 ipcRenderer.on('file-processed', (event, file, content) => {
-  selectedfile.innerHTML = file
-  userlist.innerHTML = content
+  let users = content.split('\n')
+
+  if (users && users.length > 0) {
+    users.map((user) => {
+      if (user.trim()) {
+        let user_json = JSON.parse(user)
+        let div = document.createElement('div')
+
+        div.innerHTML = user_json.name
+
+        userlist.appendChild(div)
+      }
+    })
+
+    selectedfile.innerHTML = `selected file: ${file} - ${users.length} results`
+    output.style.display = 'block'
+  }
 })
 
 ipcRenderer.on('file-saved', (event, content) => {
